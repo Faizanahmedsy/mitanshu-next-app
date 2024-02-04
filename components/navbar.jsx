e16f7ad4b6dbb85350481aee703e8cf49eeb3a50
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-02-04 17:17:19 +0530

    basic website

---
+++ components/navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center bg-zinc-600 h-20 p-5">
+      <div className="font-bold">Logo</div>
+      <div className="flex justify-between gap-6">
+        <div>llink</div>
+        <div>llink</div>
+        <div>llink</div>
+        <div>llink</div>
+        <div>llink</div>
+        <div>llink</div>
+      </div>
+      <div>Sign In</div>
+    </div>
+  );
+};
+
+export default Navbar;
